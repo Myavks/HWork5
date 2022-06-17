cd5174e65aedf87ecfdd2e513c66445854977bcd
Myavks <ikuira@mail.ru>
2022-06-17 15:21:01 +0300

    Добавьте файлы проекта.

---
+++ FirstTaask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FirstTaask
+{
+    class Program
+    {
+        static void Print(string[] args)
+        {
+            foreach (string str in args)
+            {
+                Console.WriteLine(str);
+            }
+        }
+        static string[] SplittingOnWords(string s)
+        {
+            return s.Split();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string sentence = Convert.ToString(Console.ReadLine());
+            Print(SplittingOnWords(sentence));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SecondTask5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask5
+{
+    class Program
+    {
+        static string[] ReversWords(string s)
+        {
+            string[] newArray = SplittingOnWords(s).Reverse().ToArray();
+            return newArray;
+        }
+        static string[] SplittingOnWords(string s)
+        {
+            return s.Split();
+        }
+        static void PrintArray(string[] array)
+        {
+            string resultString="";
+            foreach (string newString in array)
+            {
+                resultString += newString + " ";
+            }
+            Console.WriteLine(resultString);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string sentence = Convert.ToString(Console.ReadLine());
+            string[] reversArray = ReversWords(sentence);
+            PrintArray(reversArray);
+            Console.ReadKey();
+        }
+    }
+}
